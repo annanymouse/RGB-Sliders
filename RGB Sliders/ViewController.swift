6f9080fa5999c561d32694a1311f579175231e5c
Anna Hull <annachull@gmail.com>
2015-10-01 22:33:41 -0700

    Initial Commit

---
+++ RGB Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Sliders
+//
+//  Created by Anna Hull on 10/1/15.
+//  Copyright © 2015 Anna Hull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
